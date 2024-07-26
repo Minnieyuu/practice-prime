8df3f4d442fbaae7b0d11289d1e291b1cc5bbfbe
minnieYu <minnieyu@uitctech.com.tw>
2024-07-26 17:46:20 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 import App from './App.vue'
 import router from './router'
+import Button from 'primevue/button'
 
 const app = createApp(App)
-
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
+app.component('Button', Button)
 app.use(createPinia())
 app.use(router)
 
